c353e5a53bbe1df2607575469136df58f63df7f2
HARJOT666 <harjotbagga66@gmail.com>
2026-09-17 20:28:12 +0530

    Sync: 0121. Best Time to Buy and Sell Stock (Java)

--- 0121-best-time-to-buy-and-sell-stock/README.md
+++ 0121-best-time-to-buy-and-sell-stock/README.md
@@ -37,7 +37,7 @@ Explanation: In this case, no transactions are done and the max profit = 0.
 ## Solution
 
 - **Language:** Java
-- **Runtime:** 59 ms
-- **Memory:** 113.7 MB
+- **Runtime:** 56 ms
+- **Memory:** 115.3 MB
 
-> Synced automatically from an Accepted LeetCode submission on 2026-09-15.
+> Synced automatically from an Accepted LeetCode submission on 2026-09-17.
--- 0121-best-time-to-buy-and-sell-stock/Solution.java
+++ 0121-best-time-to-buy-and-sell-stock/Solution.java
@@ -1,47 +1,40 @@
 class Solution {
-
     public int maxProfit(int[] prices) {
+        int n = prices.length;
 
-        int[][] dp = new int[prices.length][2];
-
-        for (int i = 0; i < prices.length; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[n+1][2];
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0] = -1;
+            dp[i][1] = -1;
         }
-
-        return bsdka(0, prices, true, dp);
+        int answer = bsdks(0,dp,n,0,prices);
+        return Math.max(0,answer);
     }
-
-    public int bsdka(int days, int[] prices, boolean canBuy, int[][] dp) {
-
-        if (days == prices.length) {
+    public int bsdks(int days,int[][] dp,int n,int hold,int[] prices){
+        if(days == n){
             return 0;
         }
-
-        int state = canBuy ? 1 : 0;
-
-        if (dp[days][state] != -1) {
-            return dp[days][state];
+        
+        if(dp[days][hold] != -1){
+            return dp[days][hold];
         }
-
-        if (canBuy) {
-
-            int buy = -prices[days] +
-                      bsdka(days + 1, prices, false, dp);
-
-            int hold = bsdka(days + 1, prices, true, dp);
-
-            dp[days][state] = Math.max(buy, hold);
+        
+        //i have two options either buy or sell
+        //days cannot be same obviously
+        
+        if(hold == 0){
+             int buy = -prices[days] + bsdks(days+1,dp,n,1,prices);
+             int skip = bsdks(days+1,dp,n,0,prices);
+             dp[days][hold] = Math.max(buy,skip);
         }
-
-        else {
-
-            int sell = prices[days];
-
-            int hold = bsdka(days + 1, prices, false, dp);
-
-            dp[days][state] = Math.max(sell, hold);
+        else{
+            int sell = prices[days] ;
+            int skip = bsdks(days+1,dp,n,1,prices);
+            dp[days][hold] = Math.max(sell,skip);
         }
-
-        return dp[days][state];
+        
+       
+        return dp[days][hold];
     }
 }
